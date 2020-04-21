f5c1fb73d9115e1808e90ffff96c859d66bf6583
ikhwan12 <ikhwan.ikhwan52@gmail.com>
2020-04-21 03:10:28 +0000

    init commit

---
+++ start.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export PATH=$PATH:/usr/local/cuda-10.0/bin
+export CUDADIR=/usr/local/cuda-10.0
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.0/lib64
+
+export PATH=$PATH:/usr/local/cuda-10.1/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda-10.1/lib64
